379953c6949f688aef5c31c76a2f80d60d92d4f6
Phil Zona <phil.zona@vercel.com>
2024-05-06 17:51:30 -0400

    Add some comments

--- components/redirector.tsx
+++ components/redirector.tsx
@@ -3,6 +3,8 @@
 import { useRedirectWithQuery } from '@/hooks/useRedirectWithQuery';
 
 export const Redirector = ({ href }: { href: string }) => {
+  // Need to use the hook here rather than in /[slug]/page.tsx because
+  // it must be a client component due to using useSearchParams()
   const { redirectWithQuery } = useRedirectWithQuery();
 
   redirectWithQuery(href);
--- hooks/useRedirectWithQuery.ts
+++ hooks/useRedirectWithQuery.ts
@@ -1,13 +1,16 @@
 import { redirect, useSearchParams } from 'next/navigation';
 
 export const useRedirectWithQuery = () => {
+  // Get query params on the client side
   const params = useSearchParams();
 
+  // Create new query string
   let destination = `?`;
   params.forEach((value: string, key: string) => {
     destination = `${destination}${key}=${value}&`;
   });
 
+  // Redirect function that joins the original link + the new query string
   const redirectWithQuery = (href: string) => {
     redirect(href + destination.slice(0, -1));
   };
